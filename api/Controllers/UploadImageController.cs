64194b902e0c3a8790e660bf83263565752bbb75
Rachel Kelly <rachel.kelly@softwire.com>
2025-06-25 14:16:58 +0100

    error fixed, fetch request working and url successfully stored in DB, README updated with instructions

--- api/Controllers/UploadImageController.cs
+++ api/Controllers/UploadImageController.cs
@@ -12,16 +12,13 @@ public class UploadImageController(WhaleSpottingDbContext context)  : Controller
     private readonly WhaleSpottingDbContext _context = context;
 
 
-        [HttpPost("store-image-url")]
+        [HttpPost("/store-image-url")]
         public async Task<IActionResult> StoreImageUrl([FromBody] UploadedImages image)
         {
-        Console.WriteLine("in the controller");
             if (string.IsNullOrEmpty(image.Public_Id) || string.IsNullOrEmpty(image.Image_URL))
         {
-             Console.WriteLine("in the controller: bad request");
             return BadRequest(new { error = "Missing public_id or image_url" });
         }
- Console.WriteLine("in the controller: good request");
             _context.UploadedImages.Add(image);
             await _context.SaveChangesAsync();
 
--- api/Models/DatabaseModels/UploadedImages.cs
+++ api/Models/DatabaseModels/UploadedImages.cs
@@ -7,4 +7,6 @@ public class UploadedImages
     public int Id { get; set; }
     public required string Public_Id { get; set; }
     public required string Image_URL { get; set; }
+
+    //The idea is that will not be its own table but will ultimately be part of the whole report table.
 }
