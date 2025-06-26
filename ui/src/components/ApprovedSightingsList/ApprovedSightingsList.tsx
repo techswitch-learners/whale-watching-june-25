0d22bd0fb0e09e1fb6f8932d0f621d5e0b7f03b6
Rachel Kelly <rachel.kelly@softwire.com>
2025-06-26 16:02:37 +0100

    required information from sighting report table  displayed (not styled)

--- ui/src/components/ApprovedSightingsList/ApprovedSightingsList.tsx
+++ ui/src/components/ApprovedSightingsList/ApprovedSightingsList.tsx
@@ -1,7 +1,5 @@
 import React, {useState, useEffect } from "react";
 import "./ApprovedSightingsList.scss";
-// import {allSightings} from '..api\Controllers\SightingReportsController.cs'
-
 import { fetchSightings, SightingReport } from "../../../api/ApiClient";
 
 export function ApprovedSightingsList() {
@@ -21,7 +19,12 @@ const ApprovedSightings = sightings.filter(sighting => sighting.status === 'appr
      <>
      {ApprovedSightings.length > 0 ? (
          ApprovedSightings.map((sightingReport: SightingReport) => (
-        <p key={sightingReport.userId}>{sightingReport.description}</p>
+      <>
+            <p>Date of sighting: {new Date(sightingReport.dateOfSighting).toString()}</p>
+             <p>Sighting latitude: {sightingReport.latitude} Sighting longitude:{sightingReport.longitude}</p>
+            <p>Sighting description: {sightingReport.description}</p>
+            <p>Whale species:{sightingReport.speciesId}</p></> //need to link species id to whale table for more info?
+            //image to go here 
         ))
         ) : (
         <p>No approved sightings</p>
@@ -29,4 +32,42 @@ const ApprovedSightings = sightings.filter(sighting => sighting.status === 'appr
         </>
         );
     }
+
+
+
+
+
+//Why can't we use the return from get all sightings endpoint for this? Why do we do a new fetch in api client?
+
+// import React, {useState, useEffect } from "react";
+// import "./ApprovedSightingsList.scss";
+// import {allSightings} from '..api\Controllers\SightingReportsController.cs'
+// import { fetchSightings, SightingReport } from "../../../api/ApiClient";
+
+// export function ApprovedSightingsList(allSightings: any[]) {
+
+//     // const [sightings, setSightings] = useState<SightingReport[]>([]);
+
+//     // useEffect(() => {
+//     //     fetchSightings().then((response) => {
+//     //         setSightings(response);
+//     //     });
+//     // }, []);
+
+//     console.log(allSightings)
+
+// const ApprovedSightings = allSightings.filter(sighting => sighting.status === 'approved');
+    
+//     return (
+//      <>
+//      {ApprovedSightings.length > 0 ? (
+//          ApprovedSightings.map((sighting) => (
+//         <p key={sighting.userId}>{sighting.description}</p>
+//         ))
+//         ) : (
+//         <p>No approved sightings</p>
+//             )}
+//         </>
+//         );
+//     }
 
