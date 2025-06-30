99b995f69be3dfbbc8e26cc790366ab2f31f4fd3
Hannah Dunsmore <hannah.dunsmore@softwire.com>
2025-06-30 11:35:08 +0100

    refactored sea location data, added key to tr

--- ui/api/ApiClient.ts
+++ ui/api/ApiClient.ts
@@ -1,4 +1,5 @@
 export interface SightingReport {
+    id: number;
     description: string;
     dateOfSighting: Date;
     longitude: number;
@@ -12,4 +13,17 @@ export async function fetchSightings(): Promise<SightingReport[]> {
     const response = await fetch(`http://localhost:5067/sightingreports/all`);
     const data = await response.json();
     return data;
+}
+
+const geonamesUsername = import.meta.env.VITE_GEONAMES_USERNAMES;
+
+export async function fetchSeaLocation(latitude: number, longitude:number){
+    const response = await fetch(`http://api.geonames.org/oceanJSON?lat=${latitude}&lng=${longitude}&username=${geonamesUsername}`);
+        if (response.ok)
+        {
+            const data = await response.json();
+            return data?.ocean?.name || "Unknown";
+        } else {
+            return "Unknown";
+        }
 }
--- ui/src/components/ApprovedSightingsList/ApprovedSightingsList.tsx
+++ ui/src/components/ApprovedSightingsList/ApprovedSightingsList.tsx
@@ -1,14 +1,13 @@
 import React, {useState, useEffect } from "react";
 import "./ApprovedSightingsList.scss";
-import { fetchSightings, SightingReport } from "../../../api/ApiClient";
+import { fetchSightings, SightingReport, fetchSeaLocation } from "../../../api/ApiClient";
 import {format} from 'date-fns';
 
 
 export function ApprovedSightingsList() {
 
     const [sightings, setSightings] = useState<SightingReport[]>([]);
-    const [seaData, setSeaData] = useState<Map<Date, string>>(new Map());
-    const geonamesUsername = import.meta.env.VITE_GEONAMES_USERNAMES;
+    const [seaData, setSeaData] = useState<Map<number, string>>(new Map());
 
     useEffect(() => {
         fetchSightings().then((response) => {
@@ -20,34 +19,18 @@ export function ApprovedSightingsList() {
 
     useEffect(() => {
         if (!sightings) return;
-
-        const fetchSeaData = async (latitude: number, longitude:number) => {
-            const response = await fetch(`http://api.geonames.org/oceanJSON?lat=${latitude}&lng=${longitude}&username=${geonamesUsername}`);
-            if (response.ok)
-            {
-                const data = await response.json();
-                return data?.ocean?.name || "Unknown";
-            } else {
-                return "Unknown";
-            }
-        }
-
         sightings.forEach(sighting => {
-        fetchSeaData(sighting.latitude, sighting.longitude).then(response =>
+        fetchSeaLocation(sighting.latitude, sighting.longitude).then(response =>
         {
             setSeaData(prev => {
                 const updated = new Map(prev);
-                updated.set(sighting.dateOfSighting, response);
+                updated.set(sighting.id, response);
                 return updated;
             })
         }
         );
         })
     }, [sightings]);
-
-    // const ApprovedSightings = sightings
-    //     .filter(sighting => sighting.status === 'approved')
-    //     .sort((a, b) => new Date(b.dateOfSighting).getDate() - new Date(a.dateOfSighting).getDate());
     
     return (
      <>
@@ -58,20 +41,22 @@ export function ApprovedSightingsList() {
             <div className="sighting-container">
                 <table className= "approved-sightings-table">
                     <thead className="approved-sightings-table-header">
-                        <th>Date of sighting: </th>
-                        <th>Species: </th>
-                        <th>Location: </th>
-                        <th>Sighting latitude: </th>
-                        <th>Sighting longitude: </th>
-                        <th>Username: </th>
-                        <th>Description: </th>
+                        <tr>
+                            <th>Date of sighting: </th>
+                            <th>Species: </th>
+                            <th>Location: </th>
+                            <th>Latitude: </th>
+                            <th>Longitude: </th>
+                            <th>Username: </th>
+                            <th>Description: </th>
+                        </tr>
                     </thead>
                     <tbody>
                         {sightings.map((sightingReport: SightingReport) => 
-                            <tr>
+                            <tr key={sightingReport.id}>
                                 <td>{format(new Date(sightingReport.dateOfSighting), 'dd-MM-yyyy')}</td>
                                 <td>{sightingReport.speciesId}</td>
-                                <td>{seaData.get(sightingReport.dateOfSighting)}</td>
+                                <td>{seaData.get(sightingReport.id)}</td>
                                 <td>{sightingReport.latitude}</td>
                                 <td>{sightingReport.longitude}</td>
                                 <td>{sightingReport.userId}</td>
@@ -83,7 +68,7 @@ export function ApprovedSightingsList() {
             </div>
             </> 
         ) : (
-        <p>No approved sightings</p>
+        <h3 className="no-results-header">No approved sightings</h3>
             )}
     </div>
         </>
