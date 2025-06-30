db33c0bbadc96b078be0934f6015ed9e99817ba2
Rachel Kelly <rachel.kelly@softwire.com>
2025-06-30 15:07:54 +0100

    created api client and added fetch reequest to get all reports endpoint

---
+++ ui/api/ApiClient.ts
@@ -0,0 +1,16 @@
+export interface SightingReport {
+    id: number;
+    description: string;
+    dateOfSighting: Date;
+    longitude: number;
+    latitude: number;
+    speciesId: number;
+    userId: number;  
+    status: string;
+}
+
+export async function fetchSightings(): Promise<SightingReport[]> {
+    const response = await fetch(`http://localhost:5067/sightingreports/all`);
+    const data = await response.json();
+    return data;
+}
