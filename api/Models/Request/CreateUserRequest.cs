12da9cbb8b278f6ede183df6a5dff5eaf3748dad
preek0719 <preetha.krishnan@softwire.com>
2025-06-30 12:29:23 +0100

    Create new user

--- api/Models/Request/CreateUserRequest.cs
+++ api/Models/Request/CreateUserRequest.cs
@@ -14,9 +14,11 @@ public class CreateUserRequest
     public string? Email { get; set; }
 
     [Required]
+    [DataType(DataType.Password)]
     public string? Password { get; set; }
 
     [Required]
+    [DataType(DataType.Password)]
     [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
     public string? ConfirmPassword { get; set; }
 
