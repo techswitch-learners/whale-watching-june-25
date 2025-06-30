0275a8f43ff19f2cbd57a3fe3b77727a9581c242
Hannah Dunsmore <hannah.dunsmore@softwire.com>
2025-06-30 13:35:02 +0100

    fixed lint errors

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.scss'
-import CustomButton from './components/formComponents/customButton/CustomButton'
 import { ApprovedSightingsList } from './components/ApprovedSightingsList/ApprovedSightingsList'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
