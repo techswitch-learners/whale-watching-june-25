795fd666ea84a341fb5931b23bb83e0e41618ebb
Rachel Kelly <rachel.kelly@softwire.com>
2025-06-30 15:19:39 +0100

    filtered sightings for approved sightings

--- ui/src/components/formComponents/Statistics/Statistics.tsx
+++ ui/src/components/formComponents/Statistics/Statistics.tsx
@@ -1,2 +1,17 @@
+// import {fetchSightings} from "../../../can't find api client?"
+
+export function Statistics () {
+
+ const [sightings, setSightings] = useState<SightingReport[]>([]);
 
 
+ //we could pass sightings from the approved sightings list component instead of fetching?
+
+useEffect(() => {
+        fetchSightings().then((response) => {
+            const approvedSightings = response.filter(sighting => sighting.status === 'approved')
+            .sort((a, b) => new Date(b.dateOfSighting).getDate() - new Date(a.dateOfSighting).getDate());
+            setSightings(approvedSightings); 
+        });  
+    }, []);
+}
