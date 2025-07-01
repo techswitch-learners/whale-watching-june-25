6caab2f413f9f6ca611e0e65b9b349b5477912d6
Rachel Kelly <rachel.kelly@softwire.com>
2025-07-01 14:56:25 +0100

    merge issuesaddressed

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import { CreateWhaleSighting } from './pages/WhaleSighting/CreateWhaleSighting'
+import { CreateWhaleSightingPage } from './pages/WhaleSighting/CreateWhaleSightingPage'
 import { ReactElement } from 'react';
 import { SignUp } from './pages/SignUp/SignUpPage';
  
@@ -10,7 +10,7 @@ export default function App(): ReactElement {
     return (
         <Router>
           <Routes>
-           <Route path="/add-new-sighting" element={<CreateWhaleSighting/>}/>
+           <Route path="/add-new-sighting" element={<CreateWhaleSightingPage/>}/>
            <Route path="/sign-up" element={<SignUp/>}/>      
            </Routes>        
         </Router>
