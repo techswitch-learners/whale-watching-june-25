6486732b4d49cf1468fd945e4d6bac3175de5a3e
ABarrettVH <129877088+ABarrettVH@users.noreply.github.com>
2025-07-01 10:48:57 +0100

    Add photo create report fe (#31)
    * Added functionality to UI to upload a photo
    
    * Updated backend to include image url in all form data models
    
    * Addressed review comments
    
    * front end form accepts photos from cloudinary widget
    
    * database updates
    
    * added unit tests for front end create report
    
    * Removed comment
    
    * photo url saved in database
    
    * photo url saved in database
    
    * client-side max file limit checks
    
    * lint error
    
    * Future date validation
    
    * update file format validation, and successful image upload message
    
    * validation of only one file upload per form
    
    * fixed PR review comments - #18
    
    * merged with new file structure
    
    * address git pull concerns - cleaner code and new config file
    
    * lint fix
    
    * remove comments
    
    * Update config file location
    
    ---------
    
    Co-authored-by: preek0719 <preetha.krishnan@softwire.com>
    Co-authored-by: Sathya Sreenivasan <sathya.sreenivasan@softwire.com>
    Co-authored-by: Alice Barrett <alice.barrett@softwire.com>
    Co-authored-by: Holly Knox <hk-15@users.noreply.github.com>

---
+++ ui/src/pages/WhaleSighting/CreateWhaleSightingPage.tsx
@@ -0,0 +1,14 @@
+import { JSX} from "react";
+import { Page } from "../Page/Page";
+import { CreateWhaleSightingForm } from "../../components/formComponents/WhaleSighting/CreateWhaleSightingForm";
+
+
+
+export function CreateWhaleSightingPage(): JSX.Element {
+  return (
+    <Page containerClassName="create-whale-sighting-page">
+      <h1 className="title">Submit Whale Sighting</h1>
+      <CreateWhaleSightingForm />
+    </Page>
+  );
+}
