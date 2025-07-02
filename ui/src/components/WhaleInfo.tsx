ccda87832c2c9fb0b6053ef4311b286bbc0c8d0e
Winnie Kwok <winnie.kwok@softwire.com>
2025-07-02 16:11:17 +0100

    Create a functional Whale Info page

--- ui/src/components/WhaleInfo.tsx
+++ ui/src/components/WhaleInfo.tsx
@@ -75,6 +75,10 @@ function WhaleInfo(): JSX.Element {
                     <p></p>
                 </div>
                 <div>
+                    <h3>Description</h3>
+                    <div>{selectedSpecies.Description}</div>
+                    <h3>How to find</h3>
+                    <div>{selectedSpecies.HowToFind}</div>
                     <h3>The best time and places to find a {selectedSpecies.Species}</h3>
                                         {selectedSpecies.WhenNWhereToSee.map((place, idx) => (
                                             <div key={idx}>
