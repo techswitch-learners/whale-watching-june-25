89882c2212ec8aa1d2d896633026e6a0bf5ec372
Winnie Kwok <winnie.kwok@softwire.com>
2025-07-02 11:17:35 +0100

    merge with main

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.scss'
 import LoginPage from './pages/Login/LoginPage'
 import HomePage from './pages/Home/HomePage'
