8a66021bf8b95f2105b0e7b9d2d489b32e3ef5cd
Hannah Dunsmore <hannah.dunsmore@softwire.com>
2025-07-04 10:54:48 +0100

    added logo

--- ui/src/components/Header/Header.tsx
+++ ui/src/components/Header/Header.tsx
@@ -2,11 +2,13 @@ import {NavLink} from "react-router-dom";
 import './Header.scss';
 import "../../styles/Constants.scss";
 import { useState } from "react";
+import logo from "../../assets/Images/wm-logo-sm.webp";
+
 
 export function Header() {
     return (
         <header className="header">           
-                <NavLink className="home-link" to="/">Whale Watching</NavLink>           
+                <NavLink className="home-link" to="/"><img id="whale-museum-logo" src={logo} alt="whale-museum-logo" />Whale Watching</NavLink>      
         </header>
     );
 }
@@ -23,8 +25,11 @@ export function Navbar() {
             <>
             <nav className="navbar">
             <p className="menu-text">Whale Watching</p>
-            <div className="menu-icon" onClick={handleShowNavbar}>               
-            </div> 
+            <div className="menu-icon" onClick={handleShowNavbar}>
+                {showNavbar
+                    ? <span className="icon-x">✕</span>
+                    : <span className="icon-bars">≡</span>
+                }            </div> 
             <div className={`nav-elements  ${showNavbar ? ' active' : ''}`}>
             <ul>
             <li><NavLink className="nav-link" to="/" onClick={handleShowNavbar}>Home</NavLink></li>
@@ -35,6 +40,7 @@ export function Navbar() {
              {/* <li> <NavLink className="nav-link" to="/info" onClick={handleShowNavbar} >Register</NavLink></li> */}
              <li> <NavLink className="nav-link" to="/info" onClick={handleShowNavbar}>Logout</NavLink></li>
             </ul>
+            <img id="whale-museum-logo-navbar" src={logo} alt="whale-museum-logo"></img>
             </div>
         </nav>
         </>
