2f7ce8806891934087a91974a5c2d15f27320094
preek0719 <preetha.krishnan@softwire.com>
2025-07-07 14:08:40 +0100

    fixed PR comments

--- ui/src/components/Footer/Footer.tsx
+++ ui/src/components/Footer/Footer.tsx
@@ -1,13 +1,11 @@
 import './Footer.scss';
-import "../../styles/Constants.scss";
 import logo from "../../assets/Images/wm-logo-sm.webp";
 
 export function Footer() {
     return (
-        <footer className="footer"> 
-            <p>
-            <img id="footer-whale-museum-logo" src={logo} alt="whale-museum-logo" />          
-            &nbsp; &copy; {new Date().getFullYear()} Whale Watching. All rights reserved.</p>      
+        <footer className="footer">        
+            <img id="footer-whale-museum-logo" src={logo} alt="The Whale Museum logo with an image of a black whale fin." />          
+            <p>&nbsp; &copy; {new Date().getFullYear()} Whale Watching. All rights reserved.</p>      
         </footer>
     );
 }
