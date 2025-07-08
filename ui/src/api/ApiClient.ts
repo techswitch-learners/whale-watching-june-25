3fb1619779cb5f2d2e22f51ef68619bbec986ed8
ABarrettVH <alicevanhaeften@gmail.com>
2025-07-08 11:36:16 +0100

    linting errors

--- ui/src/api/ApiClient.ts
+++ ui/src/api/ApiClient.ts
@@ -66,24 +66,6 @@ export async function fetchSpecies(): Promise<ListResponse<Species>> {
     return await response.json();
 }
 
-export async function fetchSightings(): Promise<SightingReport[]> {
-    const response = await fetch(`http://localhost:5067/sightingreports/all`);
-    const data = await response.json();
-    return data;
-}
-
-export async function fetchSeaLocation(latitude: number, longitude:number){
-    const response = await fetch(`http://api.geonames.org/oceanJSON?lat=${latitude}&lng=${longitude}&username=${geonamesUsername}`);
-
-        if (response.ok)
-        {
-            const data = await response.json();
-            return data?.ocean?.name || "Unknown";
-        } else {
-            return "Unknown";
-        }
-}
-
 export async function createUser(newUser: NewUser) {
     const response = await fetch(`http://localhost:5067/users`, {
         method: "POST",
