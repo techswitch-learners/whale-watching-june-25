80459a365d23b7a4734bb6e0bb4082038e4b5a03
ABarrettVH <alicevanhaeften@gmail.com>
2025-07-08 11:38:56 +0100

    updated with login context

--- ui/src/pages/Admin/AdminDashboardPage.tsx
+++ ui/src/pages/Admin/AdminDashboardPage.tsx
@@ -1,18 +1,19 @@
-import { JSX} from "react";
+import { JSX, useContext} from "react";
 import { Page } from "../Page/Page";
 import {ListPendingSightings  } from "../../components/Admin/ListPendingSightings";
 import "./AdminDashboardPage.scss"
+import { LoginContext } from "../../components/Login/LoginManager/LoginContext";
 
 
 export function AdminDashboardPage(): JSX.Element | null {
- // useAdminStatus();
-  //if (loginContext.isAdmin) {
+ const loginContext  = useContext(LoginContext);
+  if (loginContext.isUserAdmin) {
   return (
     <Page>
       <h1 className="title">Admin Dashboard</h1>
       <ListPendingSightings />
     </Page>
   );
- // }
+ }
   return null
 }
