cdbef040b8555bc99560dd8c076070995cb9e6a9
Sathya Sreenivasan <sathya.sreenivasan@softwire.com>
2025-07-09 10:13:17 +0100

    Filter design change

--- ui/src/components/ApprovedSightingsList/ApprovedSightingsList.tsx
+++ ui/src/components/ApprovedSightingsList/ApprovedSightingsList.tsx
@@ -1,7 +1,7 @@
-import {useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./ApprovedSightingsList.scss";
 import { fetchSightings, SightingReport, fetchSeaLocation } from "../../api/ApiClient";
-import {format} from 'date-fns';
+import { format } from 'date-fns';
 
 
 export function ApprovedSightingsList() {
@@ -14,51 +14,50 @@ export function ApprovedSightingsList() {
     const [userNameFilter, setUserNameFilter] = useState("");
     const [dateFilter, setDateFilter] = useState("");
     const [oceanFilter, setOceanFilter] = useState("");
-  // const [headerMessage, setHeaderMessage] = useState("No approved sightings");
+    // const [headerMessage, setHeaderMessage] = useState("No approved sightings");
     const [headerMessage, setHeaderMessage] = useState("Sightings loading . . .");
-    const[filtered, setFiltered] = useState<SightingReport[]>([])
+    const [filtered, setFiltered] = useState<SightingReport[]>([])
 
     useEffect(() => {
         fetchSightings().then((response) => {
             const approvedSightings = response.filter(sighting => sighting.status.toLowerCase() === 'approved')
-            .sort((a, b) => new Date(b.dateOfSighting).getDate() - new Date(a.dateOfSighting).getDate());
-            setSightings(approvedSightings); 
+                .sort((a, b) => new Date(b.dateOfSighting).getDate() - new Date(a.dateOfSighting).getDate());
+            setSightings(approvedSightings);
             if (approvedSightings.length === 0) {
                 setHeaderMessage("No approved sightings");
             }
-        });  
+        });
     }, []);
 
     useEffect(() => {
         if (!sightings) return;
         sightings.forEach(sighting => {
-        fetchSeaLocation(sighting.latitude, sighting.longitude).then(response =>
-        {
-            setSeaData(prev => {
-                const updated = new Map(prev);
-                updated.set(sighting.id, response);
-                return updated;
-            })
-        }
-        );
+            fetchSeaLocation(sighting.latitude, sighting.longitude).then(response => {
+                setSeaData(prev => {
+                    const updated = new Map(prev);
+                    updated.set(sighting.id, response);
+                    return updated;
+                })
+            }
+            );
         })
     }, [sightings]);
 
-     useEffect(() => {
+    useEffect(() => {
         const filteredResults = sightings.filter(
-        (sighting) =>
-            sighting.species.toLowerCase().includes(speciesFilter.toLowerCase()) &&
-            sighting.userName.toLowerCase().includes(userNameFilter.toLowerCase()) &&
-            (!dateFilter ||
-                format(new Date(sighting.dateOfSighting), 'dd-MM-yyyy') ===
+            (sighting) =>
+                sighting.species.toLowerCase().includes(speciesFilter.toLowerCase()) &&
+                sighting.userName.toLowerCase().includes(userNameFilter.toLowerCase()) &&
+                (!dateFilter ||
+                    format(new Date(sighting.dateOfSighting), 'dd-MM-yyyy') ===
                     format(new Date(dateFilter), 'dd-MM-yyyy')) &&
-            seaData.get(sighting.id)?.toLowerCase().includes(oceanFilter.toLowerCase())
-    );
-    setFiltered(filteredResults);
-    if (filtered.length === 0 && sightings.length > 0) {
-        setHeaderMessage("No matches")
-    }
-     },[sightings, speciesFilter, userNameFilter, dateFilter, oceanFilter, seaData, filtered])
+                seaData.get(sighting.id)?.toLowerCase().includes(oceanFilter.toLowerCase())
+        );
+        setFiltered(filteredResults);
+        if (filtered.length === 0 && sightings.length > 0) {
+            setHeaderMessage("No matches")
+        }
+    }, [sightings, speciesFilter, userNameFilter, dateFilter, oceanFilter, seaData, filtered])
 
     function handleClickShowImage(imageUrl: string) {
         setsightingImage(imageUrl);
@@ -71,74 +70,80 @@ export function ApprovedSightingsList() {
 
     return (
         <>
+            <div className="filter-heading hide-on-mobile">Filters</div>
+            <div className="filter hide-on-mobile">
+
+                <input
+                    className="input-filter hide-on-mobile"
+                    type="date"
+                    value={dateFilter}
+                    onChange={(e) => setDateFilter(e.target.value)}
+                />
+
+                <input
+                    className="input-filter hide-on-mobile"
+                    type="text"
+                    placeholder="Species"
+                    value={speciesFilter}
+                    onChange={(e) => setSpeciesFilter(e.target.value)}
+                />
+
+                <input
+                    className="input-filter hide-on-mobile"
+                    type="text"
+                    placeholder="Location"
+                    value={oceanFilter}
+                    onChange={(e) => setOceanFilter(e.target.value)}
+                />
+
+                <input
+                    className="input-filter hide-on-mobile"
+                    type="text"
+                    placeholder="User Name"
+                    value={userNameFilter}
+                    onChange={(e) => setUserNameFilter(e.target.value)}
+                />
+
+            </div>
             <div className="sighting-list-container">
-                <h2 id="sightings-list-header">Sightings</h2>
+                <h2 id="sightings-list-header">Whale Sightings</h2>
                 {sightings.length > 0 ? (
                     <>
                         <div className="sighting-container">
                             <table className="approved-sightings-table">
                                 <thead className="approved-sightings-table-header">
                                     <tr>
-                                        <th>                                         
-                                            <input
-                                            className="input-filter hide-on-mobile"
-                                            type="date"
-                                            value={dateFilter}
-                                            onChange={(e) => setDateFilter(e.target.value)}
-                                            />Date: </th>
-                                        <th>                                            
-                                            <input
-                                            className="input-filter hide-on-mobile"
-                                            type="text"
-                                            placeholder="Species Filter"
-                                            value={speciesFilter}
-                                            onChange={(e) => setSpeciesFilter(e.target.value)}
-                                            />Species: </th>
-                                        <th>
-                                            <input
-                                            className="input-filter hide-on-mobile"
-                                            type="text"
-                                            placeholder="Ocean Filter"
-                                            value={oceanFilter}
-                                            onChange={(e) => setOceanFilter(e.target.value)}
-                                            />Location: 
-                                        </th>
+                                        <th> Date: </th>
+                                        <th> Species: </th>
+                                        <th> Location:</th>
                                         <th className="hide-on-mobile">Latitude: </th>
                                         <th className="hide-on-mobile">Longitude: </th>
-                                        <th>
-                                            <input
-                                            className="input-filter hide-on-mobile"
-                                            type="text"
-                                            placeholder="User Name Filter"
-                                            value={userNameFilter}
-                                            onChange={(e) => setUserNameFilter(e.target.value)}
-                                            /> Username:  
-                                        </th>
+                                        <th> Username: </th>
                                         <th className="hide-on-mobile">Description: </th>
                                         <th className="hide-on-mobile">Photo: </th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     {filtered.length > 0 ? (
-                                    filtered.map((sightingReport: SightingReport) =>
-                                        <tr key={sightingReport.id}>
-                                            <td>{format(new Date(sightingReport.dateOfSighting), 'dd-MM-yyyy')}</td>
-                                            <td>{sightingReport.species}</td>
-                                            <td>{seaData.get(sightingReport.id)}</td>
-                                            <td className="hide-on-mobile">{sightingReport.latitude}</td>
-                                            <td className="hide-on-mobile">{sightingReport.longitude}</td>
-                                            <td>{sightingReport.userName}</td>
-                                            <td className="hide-on-mobile">{sightingReport.description}</td>
-                                            <td className="hide-on-mobile">{sightingReport.imageUrl != null ? <button className="view-photo-button" onClick={() => handleClickShowImage(sightingReport.imageUrl)}>View</button> : <p>No photo available</p>}</td>
+                                        filtered.map((sightingReport: SightingReport) =>
+                                            <tr key={sightingReport.id}>
+                                                <td>{format(new Date(sightingReport.dateOfSighting), 'dd-MM-yyyy')}</td>
+                                                <td>{sightingReport.species}</td>
+                                                <td>{seaData.get(sightingReport.id)}</td>
+                                                <td className="hide-on-mobile">{sightingReport.latitude}</td>
+                                                <td className="hide-on-mobile">{sightingReport.longitude}</td>
+                                                <td>{sightingReport.userName}</td>
+                                                <td className="hide-on-mobile">{sightingReport.description}</td>
+                                                <td className="hide-on-mobile">{sightingReport.imageUrl != null ? <button className="view-photo-button" onClick={() => handleClickShowImage(sightingReport.imageUrl)}>View</button> : <p>No photo available</p>}</td>
+                                            </tr>
+                                        )
+                                    ) : (
+                                        <tr>
+                                            <td className="no-results-header" colSpan={8}>{headerMessage}</td>
                                         </tr>
-                                    )
-                                ) : (
-                                    <tr>
-                                    <td className="no-results-header" colSpan={8}>{headerMessage}</td>
-                                    </tr>
-                                )}
+                                    )}
                                 </tbody>
-                                    
+
                             </table>
                         </div>
                         {showSightingImage && <div className="sighting-report-photo-container">
