9765de1dea4ee1817b0212a62543ae5f8454f742
preek0719 <preetha.krishnan@softwire.com>
2025-07-10 12:11:31 +0100

    removed method from controller

--- api/Controllers/SpeciesController.cs
+++ api/Controllers/SpeciesController.cs
@@ -15,14 +15,6 @@ public class SpeciesController : ControllerBase
         _whaleSpeciesRepository = whaleSpeciesRepository;
     }
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<WhaleSpecies>>> GetWhales()
-    {
-        var result = await _whaleSpeciesRepository.GetWhaleSpecies();
-
-        return Ok(result);
-    }
-
     
     [HttpGet("by-name/{species}")]
     public async Task<ActionResult<WhaleSpecies>> GetWhalesBySpecies(string species)
