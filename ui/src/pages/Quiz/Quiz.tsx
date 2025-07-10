2c57c0e86ab5ff6b3041454bd076ab383f2fc924
Sathya Sreenivasan <sathya.sreenivasan@softwire.com>
2025-07-10 15:05:01 +0100

    Style Changes

--- ui/src/pages/Quiz/Quiz.tsx
+++ ui/src/pages/Quiz/Quiz.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import './Quiz.scss';
 import QnA from '../../components/QnA/QnA';
 import WhaleQuiz from '../../assets/what_kind_of_whale_are_you_quiz.json';
+import { Page } from '../Page/Page';
 
 const Quiz: React.FunctionComponent = () => {
 
     return (
-        <div>
-            <p className='QuizTitle'>🐋 What Whale Are You?</p>
-            <QnA QuestionSet={WhaleQuiz}/>
-        </div>
+        <Page>
+            <div>
+                <p className='QuizTitle'>🐋 What Whale Are You?</p>
+                <QnA QuestionSet={WhaleQuiz}/>
+            </div>
+        </Page>
     );
 }
 
